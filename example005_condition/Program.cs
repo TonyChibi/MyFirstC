192acb2815bf678e0e16348dfb4ed9839c1939ba
GlazHorsez <to5gogol@gmail.com>
2023-05-20 00:52:23 +0300

    add Example 005 Condition

---
+++ example005_condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("insert user name ");
+string username= Console.ReadLine();
+
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
