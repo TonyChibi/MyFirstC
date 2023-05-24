0f46e7ebc98d0fdeca7c21f350bd9f588929b2e6
GlazHorsez <to5gogol@gmail.com>
2023-05-25 01:26:14 +0300

    add example 010

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int []arr={1,2,3,4,5,6,7,4,8,124,9};
+int n=arr.Length;
+int find=4;
+int index=0;
+while(index<n){
+
+    if (arr[index]==find){
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
