72c1edd39882d2cd5dfa7ba2e6c4e3647eeaf41d
GlazHorsez <to5gogol@gmail.com>
2023-05-25 01:53:00 +0300

    add example 011 array adn methods assambly

---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+void FillArray(int [] collection){
+
+    int length=collection.Length;
+    int index=0;
+    while (index<length)
+    {
+        collection[index]=new Random().Next(1,10);
+        index++;
+    }
+
+}
+
+void PrintArray(int []coll){
+    int count=coll.Length;
+    int pos=0;
+    while(pos<count){
+        Console.WriteLine(coll[pos]);
+        pos++;
+    }
+}
+
+int IndexOf(int [] coll, int find){
+
+    int count = coll.Length;
+    int index=0;
+    int position=-1;
+    while(index<count){
+        if(coll[index]==find){
+            position=index;
+            break;
+        }
+        index++;
+    }
+     return position;
+}
+
+int []arr= new int [10]; 
+
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+int pos = IndexOf(arr,4);
+Console.WriteLine(pos);
